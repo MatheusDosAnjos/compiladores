e7dfccbcde535455d4b1812685be7dfb1863e4bd
Matheus dos Anjos <matheus.anjos@aprix.com.br>
2025-03-25 20:38:27 -0300

    Fix files

--- etapa1/lex.yy.h
+++ etapa1/lex.yy.h
@@ -1,8 +1,16 @@
+/*
+20/03/2025
+UFRGS - INF01147 Compiladores - 2025/1
+Matheus Adam dos Anjos
+*/
+
 #ifndef LEX_YY_H
 #define LEX_YY_H
 
+#include <cstdio>
+
 extern int yylex(void);
-extern char *yytext;
-extern FILE *yyin;
+extern char* yytext;
+extern FILE* yyin;
 
 #endif
--- etapa1/main.cpp
+++ etapa1/main.cpp
@@ -1,3 +1,9 @@
+/*
+20/03/2025
+UFRGS - INF01147 Compiladores - 2025/1
+Matheus Adam dos Anjos
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
