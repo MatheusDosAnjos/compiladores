d6b3ca28218844c9ffa9e355da8ed09b2ee98c3b
Matheus dos Anjos <matheus.anjos@aprix.com.br>
2025-05-01 10:34:49 -0300

    Add semantic file

---
+++ etapa4/semantic.cpp
@@ -0,0 +1,7 @@
+/*
+01/05/2025
+UFRGS - INF01147 Compiladores - 2025/1
+Matheus Adam dos Anjos
+*/
+
+#include "semantic.hpp"
---
+++ etapa4/semantic.hpp
@@ -0,0 +1,10 @@
+/*
+01/05/2025
+UFRGS - INF01147 Compiladores - 2025/1
+Matheus Adam dos Anjos
+*/
+
+#ifndef SEMANTIC_HPP
+#define SEMANTIC_HPP
+
+#endif
