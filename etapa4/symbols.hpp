fcbb049d499df03e24c9f049b1a82e3d8893b230
Matheus dos Anjos <matheus.anjos@aprix.com.br>
2025-05-01 10:31:15 -0300

    Add SymbolDataType

--- etapa4/symbols.hpp
+++ etapa4/symbols.hpp
@@ -13,8 +13,13 @@ enum class SymbolType {
     IDENTIFIER, INT, CHAR, REAL, STRING
 };
 
+enum class SymbolDataType {
+    INT, CHAR, REAL, STRING
+};
+
 struct Symbol {
     SymbolType type;
+    SymbolDataType dataType;
     std::string text;
     public: Symbol(SymbolType t, std::string s): type(t), text(s) {}
 };
