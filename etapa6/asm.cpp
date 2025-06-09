95ea89dd593fb2b55bdc183da5d2152efb36d21d
Matheus dos Anjos <matheus.anjos@aprix.com.br>
2025-06-08 22:30:24 -0300

    Add BEGIN and END FUNC asm

--- etapa6/asm.cpp
+++ etapa6/asm.cpp
@@ -1,13 +1,42 @@
 /*
-03/06/2025
+27/06/2025
 UFRGS - INF01147 Compiladores - 2025/1
 Matheus Adam dos Anjos
 */
 
 #include <string>
+#include <sstream>
 #include "ast.hpp"
 #include "tac.hpp"
 
 using namespace std;
 
-string generateAsm(Tac* tacList, AstNode* root) {}
+string generateDataSection(AstNode* root) {
+    stringstream ss;
+    ss << "    .section    .rodata\n";
+        
+    return ss.str();
+}
+
+string generateAsm(Tac* tacList, AstNode* root) {
+    stringstream ss;
+
+    string dataSection = generateDataSection(root);
+
+    for (; tacList; tacList = tacList->next) {
+        switch (tacList->type) {
+            case TacType::BEGIN_FUNC:
+                ss << "    .globl    " << tacList->res->text << "\n";
+                ss << tacList->res->text << ":\n";
+                ss << "    pushq    %rbp\n";
+                ss << "    movq    %rsp, %rbp\n";
+                break;
+            case TacType::END_FUNC:
+                ss << "    popq    %rbp\n";
+                ss << "    ret\n";
+                break;
+        }
+    }
+
+    return ss.str();
+}
